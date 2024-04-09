d5267e12bb85ab2316f9b410d42750c140a23f13
Frantz Chery <frantzchery@hotmail.com>
2024-04-09 10:21:53 -0400

    Completed sections 1 and 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// 1. Variables & Data types
+//! SECTION #1
 
-// A. Variables
+//+ A. Variables
 
 // Declare a new constant variable called myName and assign it your name?
 
@@ -21,7 +21,7 @@ console.log(
   `Hello, my name is ${myName} and my favorite programming language is ${favoriteLanguage}. `
 );
 
-// B. Strings
+//+ B. Strings
 // Create a variable called firstVariable
 // Assign it the value of the string "Hello World"
 // Change the value of this variable to some number
@@ -44,7 +44,7 @@ let whatUp = 'Howdy, my name is ' + yourName;
 
 console.log(whatUp);
 
-// C. Booleans
+//+ C. Booleans
 // Using the provided variable definitions, replace the blanks so that all log statements print true in the console.
 // Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console:
 
@@ -65,3 +65,61 @@ console.log(e === 'Kevin');
 console.log(a < b) === b < c; // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
 console.log(a + a !== d); // note: the answer is a simple arithmetic equation, not something "weird"
 console.log(48 !== '48');
+
+//+ D. The farm
+// Declare a variable animal. Set it to be either "cow" or something else
+// Write code that will print out "mooooo" if the it is equal to cow
+// Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow." Commit
+
+let animal = 'cow';
+
+if (animal === 'cow') {
+  console.log('mooooo');
+} else {
+  console.log("Hey! You're not a cow.");
+}
+
+//+ E. Driver's Ed
+// Make a variable that holds a person's age; be semantic
+// Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+
+let personAge = 16;
+
+if (personAge >= 16) {
+  console.log('Here are the keys!');
+} else {
+  console.log("Sorry, you're too young. Take a Hike!");
+}
+
+//! SECTION #3
+
+//+ F. Biggie Smalls
+// Create a variable that contains an integer.
+
+// Write an if ... else statement that:
+// console.log() "little number" if the number is entered is less than 100
+// console.log()  big number if the number is greater than or equal to 100.
+
+let number = 75;
+
+if (number < 100) {
+  console.log('little number');
+} else {
+  console.log('big number');
+}
+
+//+ G. Monkey in the Middle
+// Write an if ... else if ... else statement:
+// console.log() "little number" if the number entered is less than 5.
+// If the number entered is more than 10, log "big number".
+// Otherwise, log "monkey".
+
+let digit = 7;
+
+if (digit < 5) {
+  console.log('little number');
+} else if (digit > 10) {
+  console.log('big number');
+} else {
+  console.log('monkey');
+}
